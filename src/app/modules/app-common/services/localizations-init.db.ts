3331d8e4b772944794c63f099eb883703cc0b7bc
Elian ORIOU <eoriou@gmail.com>
2019-10-27 14:27:58 +0100

    add another localization

--- src/app/modules/app-common/services/localizations-init.db.ts
+++ src/app/modules/app-common/services/localizations-init.db.ts
@@ -5,5 +5,15 @@ export const localizations: Localization[] = [
         name: 'Bordeaux',
         latitude: '44.839263',
         longitude: '-0.593534'
+    },
+    {
+        name: 'Paris',
+        latitude: '48.856614',
+        longitude: '2.3522219'
+    },
+    {
+        name: 'Toulouse',
+        latitude: '43.6043',
+        longitude: '1.4437'
     }
 ];
