6599f091598ffae564c8e2485db679412325bbb6
driccio <damien.riccio@4sh.fr>
2019-10-28 07:45:29 +0100

    add cities with better data

--- src/app/modules/app-common/services/localizations-init.db.ts
+++ src/app/modules/app-common/services/localizations-init.db.ts
@@ -2,6 +2,16 @@ import {Localization} from '../model/localization.model';
 
 export const localizations: Localization[] = [
     {
+        name: 'Bayonne',
+        latitude: '43.4833',
+        longitude: '-1.4833'
+    },
+    {
+        name: 'Bergerac',
+        latitude: '44.85',
+        longitude: '0.4833'
+    },
+    {
         name: 'Bordeaux',
         latitude: '44.839263',
         longitude: '-0.593534'
@@ -10,6 +20,11 @@ export const localizations: Localization[] = [
         name: 'Paris',
         latitude: '48.856614',
         longitude: '2.3522219'
+    },
+    {
+        name: 'Royan',
+        latitude: '45.6333',
+        longitude: '-1.0333'
     },
     {
         name: 'Toulouse',
