6a5c8779f91063819e10b6d7638de49821e4d83c
Elian ORIOU <eoriou@gmail.com>
2019-11-01 15:43:55 +0100

    iteration 4 - moving guards from canActivate to canLoad

--- src/app/modules/auth/guards/is-admin-user.guard.ts
+++ src/app/modules/auth/guards/is-admin-user.guard.ts
@@ -1,17 +1,17 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {CanLoad, Route, Router, UrlSegment} from '@angular/router';
 import {AuthService} from '../services/auth.service';
 import {AbstractUserGuard} from './abstract-user.guard';
 import {Observable} from 'rxjs';
 
 @Injectable()
-export class IsAdminUserGuard extends AbstractUserGuard implements CanActivate {
+export class IsAdminUserGuard extends AbstractUserGuard implements CanLoad {
 
     constructor(authService: AuthService, router: Router) {
         super(authService, router, 'IsAdminUserGuard', ['ADMIN']);
     }
 
-    public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-        return this.check(state.url);
+    public canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> {
+        return this.check(route.path);
     }
 }
