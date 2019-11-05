66eaceaa5768597f594bb29cc189840db46973e6
driccio <damien.riccio@4sh.fr>
2019-11-05 12:35:58 +0100

    Implement specific admin dislay

--- src/app/modules/app/app.component.ts
+++ src/app/modules/app/app.component.ts
@@ -1,10 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../auth/model/user.model';
+import {AuthService} from '../auth/services/auth.service';
+import {filter} from 'rxjs/operators';
 
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    public user: User;
 
+    constructor(private authService: AuthService) {
+    }
+
+    ngOnInit() {
+        this.authService.getUsersEvents()
+            .pipe(filter(user => user !== undefined))
+            .subscribe(user => this.user = user);
+    }
 }
